243419fd6fea4e3a2479257c4930e3fe11bd90b0
JohnRoss Doherty <dohertyjr0@icloud.com>
2025-04-23 13:13:08 +0100

    added subtraction test and tests for negative

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CalculatorTest {
     Calculator calculator = new Calculator();
@@ -9,3 +10,13 @@ public class CalculatorTest {
     void testAddition() {
         assertEquals(5, calculator.add(2, 3));
 }
+
+    @Test
+    void testSubtraction() {
+        assertEquals(3, calculator.subtract(5, 2));
+    }
+
+    @Test
+    void testSubtractionThrowsOnNegative() {
+        assertThrows(IllegalStateException.class, () -> calculator.subtract(2, 5));
+    }
