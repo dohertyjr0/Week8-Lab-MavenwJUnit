9f7c38c1fbff2bde0e34ab3f37af3724e664150f
JohnRoss Doherty <dohertyjr0@icloud.com>
2025-04-23 13:20:02 +0100

    added multiplication test for overflow

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -20,3 +20,13 @@ public class CalculatorTest {
     void testSubtractionThrowsOnNegative() {
         assertThrows(IllegalStateException.class, () -> calculator.subtract(2, 5));
     }
+
+    @Test
+    void testMultiplication() {
+        assertEquals(20, calculator.multiply(4, 5));
+    }
+
+    @Test
+    void testMultiplicationOverflow() {
+        assertThrows(UnsupportedOperationException.class, () -> calculator.multiply(Integer.MAX_VALUE, 2));
+    }
